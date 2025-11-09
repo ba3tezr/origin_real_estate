ba4614cfa23ec16eb173ad93fcf67added778538
zakee <zakee.tahawi@elkhawaga.com>
2025-11-09 12:44:23 +0200

    Fix build.sh - use existing venv instead of creating new one

--- build.sh
+++ build.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 set -e
 
-echo "Creating virtual environment..."
-python3 -m venv /opt/venv
-
 echo "Activating virtual environment..."
 source /opt/venv/bin/activate
-
-echo "Upgrading pip..."
-pip install --upgrade pip
-
-echo "Installing requirements..."
-pip install -r requirements.txt
 
 echo "Running migrations..."
 python manage.py migrate --noinput
