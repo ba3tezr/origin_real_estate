04c410e26722666feed2f7ddf2b1cd3a0ade4237
zakee <zakee.tahawi@elkhawaga.com>
2025-11-09 12:51:01 +0200

    Fix root URL redirect and healthcheck path

--- config/urls.py
+++ config/urls.py
@@ -6,8 +6,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/en/', permanent=False)),
     path('admin/', admin.site.urls),
     path('i18n/', include('django.conf.urls.i18n')),
 ]
